a5b2c4d72e355ac50bfe79c9ab2d207bde10004c
rostik <rostislav.bodnaryuk@gmail.com>
2025-06-09 14:53:04 +0300

    12

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+import React, { Suspense, useContext, useState } from "react";
+import "./styles/index.scss";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "./providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { AppRoutes } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRoutes />
+      <button onClick={toggleTheme}>TOGGLE</button>
+    </div>
+  );
+};
+
+export default App;
