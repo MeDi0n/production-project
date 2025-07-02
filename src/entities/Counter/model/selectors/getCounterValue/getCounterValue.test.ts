673c8963c328a7e873a7f6bfd3f5e99dc57e8819
rostik <rostislav.bodnaryuk@gmail.com>
2025-07-02 18:31:44 +0300

    add redux store, add first entity

---
+++ src/entities/Counter/model/selectors/getCounterValue/getCounterValue.test.ts
@@ -0,0 +1,12 @@
+import { StateSchema } from "app/providers/StoreProvider";
+import { getCounterValue } from "./getCounterValue";
+import { DeepPartial } from "redux";
+
+describe("getCounterValue.test", () => {
+  test("", () => {
+    const state: DeepPartial<StateSchema> = {
+      counter: { value: 10 },
+    };
+    expect(getCounterValue(state as StateSchema)).toEqual(10);
+  });
+});
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,16 +1,9 @@
-import { BugButton } from "app/providers/ErrorBoundary";
-import { t } from "i18next";
 import { useTranslation } from "react-i18next";
 
 const MainPage = () => {
   const { t } = useTranslation();
 
-  return (
-    <div>
-      <BugButton />
-      {t("Главная")}
-    </div>
-  );
+  return <div>{t("Главная")}</div>;
 };
 
 export default MainPage;
