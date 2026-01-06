caa884128e6a9a15f0d2a26ba47e91db244027df
Rostyslav Bodnariuk <rostislav.bodnaryuk@gmail.com>
2026-01-06 15:49:01 +0200

    production

--- src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
+++ src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
@@ -1,8 +1,7 @@
-import { FC, lazy } from 'react';
-import { AddCommentFormProps } from './AddCommentForm';
+import { FC, lazy } from "react";
 
-export const AddCommentFormAsync = lazy<FC<AddCommentFormProps>>(() => new Promise((resolve) => {
-    // @ts-ignore
-    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-    setTimeout(() => resolve(import('./AddCommentForm')), 1500);
-}));
+import { AddCommentFormProps } from "./AddCommentForm";
+
+export const AddCommentFormAsync = lazy<FC<AddCommentFormProps>>(
+  () => import("./AddCommentForm")
+);
