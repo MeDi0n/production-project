1a93f93af7df2dc2a22aa107ffe822ba385959b2
Rostyslav Bodnariuk <rostislav.bodnaryuk@gmail.com>
2026-01-07 14:52:19 +0200

    add react virtualized

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -1,60 +1,111 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
-import { HTMLAttributeAnchorTarget, memo } from 'react';
-import { ArticleListItemSkeleton } from 'entities/Article/ui/ArticleListItem/ArticleListItemSkeleton';
-import { Text, TextSize } from 'shared/ui/Text/Text';
-import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
-import cls from './ArticleList.module.scss';
-import { Article, ArticleView } from '../../model/types/article';
+import { HTMLAttributeAnchorTarget, memo } from "react";
+
+import { useTranslation } from "react-i18next";
+import { List, ListRowProps, WindowScroller } from "react-virtualized";
+
+import { ArticleListItemSkeleton } from "entities/Article/ui/ArticleListItem/ArticleListItemSkeleton";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Text, TextSize } from "shared/ui/Text/Text";
+import { PAGE_ID } from "widgets/Page/Page";
+
+import { Article, ArticleView } from "../../model/types/article";
+import { ArticleListItem } from "../ArticleListItem/ArticleListItem";
+
+import cls from "./ArticleList.module.scss";
 
 interface ArticleListProps {
-    className?: string;
-    articles: Article[]
-    isLoading?: boolean;
-    target?: HTMLAttributeAnchorTarget;
-    view?: ArticleView;
+  className?: string;
+  articles: Article[];
+  isLoading?: boolean;
+  target?: HTMLAttributeAnchorTarget;
+  view?: ArticleView;
 }
 
-const getSkeletons = (view: ArticleView) => new Array(view === ArticleView.SMALL ? 9 : 3)
+const getSkeletons = (view: ArticleView) =>
+  new Array(view === ArticleView.SMALL ? 9 : 3)
     .fill(0)
     .map((item, index) => (
-        <ArticleListItemSkeleton className={cls.card} key={index} view={view} />
+      <ArticleListItemSkeleton className={cls.card} key={index} view={view} />
     ));
 
 export const ArticleList = memo((props: ArticleListProps) => {
-    const {
-        className,
-        articles,
-        view = ArticleView.SMALL,
-        isLoading,
-        target,
-    } = props;
-    const { t } = useTranslation();
+  const {
+    className,
+    articles,
+    view = ArticleView.SMALL,
+    isLoading,
+    target,
+  } = props;
 
-    const renderArticle = (article: Article) => (
+  const { t } = useTranslation();
+
+  const isBig = view === ArticleView.BIG;
+
+  const itemsPerRow = isBig ? 1 : 6;
+  const rowCount = isBig
+    ? articles.length
+    : Math.ceil(articles.length / itemsPerRow);
+
+  const rowRender = ({ index, isScrolling, key, style }: ListRowProps) => {
+    const items = [];
+    const fromIndex = index * itemsPerRow;
+    const toIndex = Math.min(fromIndex + itemsPerRow, articles.length);
+
+    for (let i = fromIndex; i < toIndex; i += 1) {
+      items.push(
         <ArticleListItem
-            article={article}
-            view={view}
-            className={cls.card}
-            key={article.id}
-            target={target}
+          article={articles[i]}
+          view={view}
+          className={cls.card}
+          target={target}
+          key={`str${i}`}
         />
-    );
-
-    if (!isLoading && !articles.length) {
-        return (
-            <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
-                <Text size={TextSize.L} title={t('Статьи не найдены')} />
-            </div>
-        );
+      );
     }
 
     return (
-        <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
-            {articles.length > 0
-                ? articles.map(renderArticle)
-                : null}
-            {isLoading && getSkeletons(view)}
-        </div>
+      <div key={key} style={style} className={cls.row}>
+        {items}
+      </div>
     );
+  };
+
+  if (!isLoading && !articles.length) {
+    return (
+      <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
+        <Text size={TextSize.L} title={t("Статьи не найдены")} />
+      </div>
+    );
+  }
+
+  return (
+    <WindowScroller scrollElement={document.getElementById(PAGE_ID) as Element}>
+      {({
+        width,
+        height,
+        registerChild,
+        onChildScroll,
+        isScrolling,
+        scrollTop,
+      }) => (
+        <div
+          ref={registerChild}
+          className={classNames(cls.ArticleList, {}, [className, cls[view]])}
+        >
+          <List
+            height={height ?? 700}
+            rowCount={rowCount}
+            rowHeight={isBig ? 700 : 330}
+            rowRenderer={rowRender}
+            width={width ? width - 80 : 700}
+            autoHeight
+            onScroll={onChildScroll}
+            isScrolling={isScrolling}
+            scrollTop={scrollTop}
+          />
+          {isLoading && getSkeletons(view)}
+        </div>
+      )}
+    </WindowScroller>
+  );
 });
