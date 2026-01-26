f40a2732a3d24a3c33909ca480b1d24c42856418
Rostyslav Bodnariuk <rostislav.bodnaryuk@gmail.com>
2026-01-26 18:42:10 +0200

    fix drawer, add browser list config

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,6 +1,9 @@
 import { useTheme } from "@/app/providers/ThemeProvider";
 import { classNames } from "@/shared/lib/classNames/classNames";
-import { useAnimationLibs } from "@/shared/lib/components/AnimationProvider/AnimationProvider";
+import {
+  AnimationProvider,
+  useAnimationLibs,
+} from "@/shared/lib/components/AnimationProvider/AnimationProvider";
 import { memo, ReactNode, useCallback, useEffect } from "react";
 import { Overlay } from "../Overlay/Overlay";
 import { Portal } from "../Portal/Portal";
@@ -93,7 +96,7 @@ export const DrawerContent = memo((props: DrawerProps) => {
   );
 });
 
-export const Drawer = memo((props: DrawerProps) => {
+const DrawerAsync = memo((props: DrawerProps) => {
   const { isLoaded } = useAnimationLibs();
 
   if (!isLoaded) {
@@ -102,3 +105,11 @@ export const Drawer = memo((props: DrawerProps) => {
 
   return <DrawerContent {...props} />;
 });
+
+export const Drawer = (props: DrawerProps) => {
+  return (
+    <AnimationProvider>
+      <DrawerAsync {...props} />;
+    </AnimationProvider>
+  );
+};
