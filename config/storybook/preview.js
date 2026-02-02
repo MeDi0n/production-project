2a39475dcf73e075b0bc71b068dac622c02c6b95
Rostyslav Bodnariuk <rostislav.bodnaryuk@gmail.com>
2026-02-02 12:16:54 +0200

    fix unit with aliases, fix storybook after refactoring

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,9 +1,11 @@
 import { addDecorator } from "@storybook/react";
+
 import { Theme } from "../../src/app/providers/ThemeProvider";
 import { RouterDecorator } from "../../src/shared/config/storybook/RouterDecorator/RouterDecorator";
 import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
 import { SuspenseDecorator } from "../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator";
 import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -12,6 +14,7 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  layout: "fullscreen",
 };
 
 addDecorator(StyleDecorator);
